37bbb2bb291b48f37668033db29eb3d331d3586f
BerhanGit <150890177+BerhanGit@users.noreply.github.com>
2024-07-10 12:24:52 +0300

    Update Program.cs

--- Delegate/Delegate/Program.cs
+++ Delegate/Delegate/Program.cs
@@ -1,40 +1,54 @@
-﻿class Program
+﻿using System;
+using System.IO;
+
+namespace DelegateExample
 {
-    delegate void DelegateMethod(string text);
-    static void Main(string[] args)
+    class Program
     {
-        Logfiles log = new Logfiles();
+        delegate void DelegateEquation(string Note, int A, int B);
 
-        DelegateMethod consoleDisplay, logFileSave, Multiple;
-
-        consoleDisplay = new DelegateMethod(log.ConsoleDisplay);
-
-        logFileSave = new DelegateMethod(log.LogFileSave);
-
-        Multiple = consoleDisplay + logFileSave;
-        Console.WriteLine("Write a Note");
-
-        var note = Console.ReadLine();
-
-        Multiple(note);
-
-        Console.ReadKey();
-
-    }
-}
-
-public class Logfiles
-{
-    public void ConsoleDisplay(string text)
-    {
-        Console.WriteLine($"Welcome to {text}. The Time is: {DateTime.Now} ");
-    }
-
-    public void LogFileSave(string text)
-    {
-        using (StreamWriter sw = new StreamWriter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Log.txt"), true))
+        static void Main(string[] args)
         {
-            sw.WriteLine($"{DateTime.Now}: {text}");
+            Math_Opreation mp = new Math_Opreation();
+
+            DelegateEquation show_Result, save_Result, multiple;
+
+            consoleDisplay = new DelegateEquation(mp.Show_Result);
+            logFileSave = new DelegateEquation(mp.Save_Result);
+
+            multiple = consoleDisplay + logFileSave;
+
+            Console.WriteLine("Write a Note:");
+            var Note = Console.ReadLine();
+
+            Console.WriteLine("Enter value for A:");
+            int A = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter value for B:");
+            int B = int.Parse(Console.ReadLine());
+
+            multiple(Note, A, B);
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Math_Opreation
+    {
+        public void Show_Result(string note, int a, int b)
+        {
+            int result = (a + b) * (a - b);
+            Console.WriteLine($"Result = ({a} + {b}) * ({a} - {b}) = {result}");
+            Console.WriteLine($"Note: {note}. The Time is: {DateTime.Now}");
+        }
+
+        public void Save_Result(string note, int a, int b)
+        {
+            int result = (a + b) * (a - b);
+            using (StreamWriter sw = new StreamWriter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Log.txt"), true))
+            {
+                sw.WriteLine($"{DateTime.Now}: Note: {note}. Result = ({a} + {b}) * ({a} - {b}) = {result}");
+            }
         }
     }
 }
