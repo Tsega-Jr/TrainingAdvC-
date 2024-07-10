95c13b5911ceb1dad8b7be89dc6ec8995e76e7e2
BerhanGit <150890177+BerhanGit@users.noreply.github.com>
2024-07-10 12:36:09 +0300

    New Maths Opreation has been Added
    New Maths Opreation has been Added

--- Delegate/Delegate/Program.cs
+++ Delegate/Delegate/Program.cs
@@ -1,54 +1,80 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
+using System;
 
-namespace DelegateExample
+class Program
 {
-    class Program
+    delegate void DelegateMethod(string text);
+    delegate void DelegateEquation(string Note, int A, int B);
+    static void Main(string[] args)
     {
-        delegate void DelegateEquation(string Note, int A, int B);
+        Logfiles log = new Logfiles();
+        Math_Opreation mp = new Math_Opreation();
 
-        static void Main(string[] args)
-        {
-            Math_Opreation mp = new Math_Opreation();
+        DelegateMethod consoleDisplay, logFileSave, Multiple;
+        DelegateEquation show_Result, save_Result, multiple;
 
-            DelegateEquation show_Result, save_Result, multiple;
+        consoleDisplay = new DelegateMethod(log.ConsoleDisplay);
 
-            consoleDisplay = new DelegateEquation(mp.Show_Result);
-            logFileSave = new DelegateEquation(mp.Save_Result);
+        logFileSave = new DelegateMethod(log.LogFileSave);
 
-            multiple = consoleDisplay + logFileSave;
+        show_Result = new DelegateEquation(mp.Show_Result);
+        save_Result = new DelegateEquation(mp.Save_Result);
 
-            Console.WriteLine("Write a Note:");
-            var Note = Console.ReadLine();
 
-            Console.WriteLine("Enter value for A:");
-            int A = int.Parse(Console.ReadLine());
+        Multiple = consoleDisplay + logFileSave;
+        multiple = show_Result + save_Result;
+        Console.WriteLine("Write a Note");
 
-            Console.WriteLine("Enter value for B:");
-            int B = int.Parse(Console.ReadLine());
+        var note = Console.ReadLine();
 
-            multiple(Note, A, B);
+        Multiple(note);
 
-            Console.ReadKey();
-        }
+        Console.WriteLine("Write a Note:");
+        var Note = Console.ReadLine();
+
+        Console.WriteLine("Enter value for A:");
+        int A = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter value for B:");
+        int B = int.Parse(Console.ReadLine());
+
+        multiple(Note, A, B);
+
+        Console.ReadKey();
+
+    }
+}
+
+public class Logfiles
+{
+    public void ConsoleDisplay(string text)
+    {
+        Console.WriteLine($"Welcome to {text}. The Time is: {DateTime.Now} ");
     }
 
-    public class Math_Opreation
+    public void LogFileSave(string text)
     {
-        public void Show_Result(string note, int a, int b)
+        using (StreamWriter sw = new StreamWriter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Log.txt"), true))
         {
-            int result = (a + b) * (a - b);
-            Console.WriteLine($"Result = ({a} + {b}) * ({a} - {b}) = {result}");
-            Console.WriteLine($"Note: {note}. The Time is: {DateTime.Now}");
+            sw.WriteLine($"{DateTime.Now}: {text}");
         }
+    }
+}
+public class Math_Opreation
+{
+    public void Show_Result(string note, int a, int b)
+    {
+        int result = (a + b) * (a - b);
+        Console.WriteLine($"Result = ({a} + {b}) * ({a} - {b}) = {note}:{result}");
 
-        public void Save_Result(string note, int a, int b)
+    }
+
+    public void Save_Result(string note, int a, int b)
+    {
+        int result = (a + b) * (a - b);
+        using (StreamWriter sw = new StreamWriter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Log.txt"), true))
         {
-            int result = (a + b) * (a - b);
-            using (StreamWriter sw = new StreamWriter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Log.txt"), true))
-            {
-                sw.WriteLine($"{DateTime.Now}: Note: {note}. Result = ({a} + {b}) * ({a} - {b}) = {result}");
-            }
+            sw.WriteLine($"{DateTime.Now}: Note: {note}. Result = ({a} + {b}) * ({a} - {b}) = {result}");
         }
     }
 }
