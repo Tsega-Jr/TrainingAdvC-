1eae3d5b536e8a242365b49259044a8a8aad3448
Simon Gezahagn <simongezahagn@gmail.com>
2024-07-10 12:19:51 +0300

    Assign1 done

--- Delegate/Delegate/Program.cs
+++ Delegate/Delegate/Program.cs
@@ -1,6 +1,10 @@
-﻿class Program
+﻿using System;
+using System.IO;
+
+class Program
 {
-    delegate void DelegateMethod(string text);
+    delegate void DelegateMethod(string text, int a, int b);
+
     static void Main(string[] args)
     {
         Logfiles log = new Logfiles();
@@ -8,33 +12,39 @@
         DelegateMethod consoleDisplay, logFileSave, Multiple;
 
         consoleDisplay = new DelegateMethod(log.ConsoleDisplay);
-
         logFileSave = new DelegateMethod(log.LogFileSave);
 
         Multiple = consoleDisplay + logFileSave;
-        Console.WriteLine("Write a Note");
 
+        Console.WriteLine("Enter a note:");
         var note = Console.ReadLine();
 
-        Multiple(note);
+        Console.WriteLine("Enter Value of A:");
+        int valueA = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter Value of B:");
+        int valueB = int.Parse(Console.ReadLine());
+
+        Multiple(note, valueA, valueB);
 
         Console.ReadKey();
-
     }
 }
 
 public class Logfiles
 {
-    public void ConsoleDisplay(string text)
+    public void ConsoleDisplay(string text, int a, int b)
     {
-        Console.WriteLine($"Welcome to {text}. The Time is: {DateTime.Now} ");
+        int result = (a + b) * (a - b);
+        Console.WriteLine($"{text}. Result = ({a} + {b}) * ({a} - {b}) = {result}. The Time is: {DateTime.Now}");
     }
 
-    public void LogFileSave(string text)
+    public void LogFileSave(string text, int a, int b)
     {
+        int result = (a + b) * (a - b);
         using (StreamWriter sw = new StreamWriter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Log.txt"), true))
         {
-            sw.WriteLine($"{DateTime.Now}: {text}");
+            sw.WriteLine($"{DateTime.Now}: {text}. Result = ({a} + {b}) * ({a} - {b}) = {result}");
         }
     }
 }
