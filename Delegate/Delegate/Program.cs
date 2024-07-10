9c7096a1cbabb0f9a176d6b40fdb9320fa2437f0
Elshaday Basie <93201203+Elshuye2112@users.noreply.github.com>
2024-07-10 12:18:58 +0300

    first
    commit

--- Delegate/Delegate/Program.cs
+++ Delegate/Delegate/Program.cs
@@ -1,24 +1,32 @@
 ﻿class Program
 {
     delegate void DelegateMethod(string text);
+    delegate void DelegateMethod1(int a,int b);
     static void Main(string[] args)
     {
         Logfiles log = new Logfiles();
-
+        
         DelegateMethod consoleDisplay, logFileSave, Multiple;
+        DelegateMethod1 sum, test;
 
-        consoleDisplay = new DelegateMethod(log.ConsoleDisplay);
-
-        logFileSave = new DelegateMethod(log.LogFileSave);
-
+       consoleDisplay = new DelegateMethod(log.ConsoleDisplay);
+ 
+       logFileSave = new DelegateMethod(log.LogFileSave);
+        sum = new DelegateMethod1(log.Sum);
+        test = new DelegateMethod1(log.Test);
         Multiple = consoleDisplay + logFileSave;
         Console.WriteLine("Write a Note");
-
         var note = Console.ReadLine();
-
+        
+        Console.WriteLine("Enter the first number");
+        int a =Convert.ToInt32( Console.ReadLine());
+        Console.WriteLine("Enter the second number");
+        int b = Convert.ToInt32(Console.ReadLine());
         Multiple(note);
+        sum(a, b);
+        test(a, b);
 
-        Console.ReadKey();
+      Console.ReadKey();
 
     }
 }
@@ -36,5 +44,13 @@ public class Logfiles
         {
             sw.WriteLine($"{DateTime.Now}: {text}");
         }
+    }
+    public void Sum(int a, int b)
+    {
+        Console.WriteLine("The sum of " + a + " and " + b + " is " + (a + b));
+    }
+    public void Test(int a, int b)
+    {
+        Console.WriteLine("The result is: " + (a + b) * (a - b));
     }
 }
