e452f112b02fc190e8a50e1bdfdd4aa2fd08ef98
Solomon S. <125763152+solasefiw@users.noreply.github.com>
2024-07-11 09:41:17 +0300

    comit

--- DetailGenerics/Program.cs
+++ DetailGenerics/Program.cs
@@ -2,22 +2,35 @@
 {
     static void Main(string[] args)
     {
-        var result = new Result { Value = 10, IsSuccess = true };
-        var display = new display { Note = "Advanced c# Generics", IsSuccess = false };
+        //var result = new Result { Value = 10, IsSuccess = true };
+        //var display = new display { Note = "Advanced c# Generics", IsSuccess = false };
+        Console.WriteLine("Enter your Ambition !!");
+        string data =Console.ReadLine();
 
-        var GenericResult = new GenericResult<string> { Value = "Generic String", IsSuccess = false };
-        var GenericResult1 = new GenericResult<int> { Value = 5, IsSuccess = false };
+        Console.WriteLine("Enter your faborite  !!");
+        int faborite = Convert.ToInt32(Console.ReadLine());
 
-        Console.WriteLine(display.Note);
-        Console.WriteLine(display.IsSuccess);
+
+
+
+        var GenericResult = new GenericResult<string,bool> { Value = data, IsSuccess = true };
+        var GenericResult1 = new GenericResult<int,bool> { Value = faborite, IsSuccess = false };
+       
+        Console.WriteLine("-------------your Ambition------------");
+        Console.WriteLine($"your best ambition is :{GenericResult.Value}");
+        Console.WriteLine($"If you hard work it will be :{GenericResult.IsSuccess}");
+        Console.WriteLine("-------------Faborite Food------------");
+        Console.WriteLine($"your faborite Food is number : {GenericResult1.Value}");
+        Console.WriteLine($"Is Exist : {GenericResult1.IsSuccess}");
+
         Console.ReadKey();
     }
 }
 
-public class GenericResult<T>
+public class GenericResult<T,B>
 {
     public T Value { get; set; }
-    public bool IsSuccess { get; set; }
+    public B IsSuccess { get; set; }
 }
 
 public class Result
